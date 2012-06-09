2802091bb2d7709074940f0882cb800ddcd1671e
Alfaniel <alfaniel.aldavan@ymail.com>
2012-06-09 09:56:22 -0400

    Request documentation.
    Added more request variables.
    
    Signed-off-by: Alfaniel <alfaniel.aldavan@ymail.com>

--- system/core/Request.php
+++ system/core/Request.php
@@ -5,46 +5,64 @@ namespace clairity;
 class Request
 {
 	/**
-	 * Request method
 	 * @var string
 	 */
 	private $_method;
 
 	/**
-	 * Headers (array of arrays or strings)
 	 * @var array
 	 */
 	private $_headers;
 
 	/**
-	 * HTTP version
 	 * @var string
 	 */
 	private $_version;
 
 	/**
-	 * Request URL
 	 * @var string
 	 */
 	private $_url;
 
 	/**
-	 * Content body
+	 * @var string
+	 */
+	private $_baseUrl;
+
+	/**
+	 * @var \clairity\Action
+	 */
+	private $_action;
+
+	/**
 	 * @var unknown_type
 	 */
 	private $_body;
 
 	/**
-	 * Session
 	 * @var Session
 	 */
 	private $_session;
 
 	/**
-	 * Cookies in the request
 	 * @var array
 	 */
 	private $_cookies;
+
+	/**
+	 * @var unknown_type
+	 */
+	private $_server;
+
+	/**
+	 * @var unknown_type
+	 */
+	private $_files;
+
+	/**
+	 * @var string
+	 */
+	private $_language;
 
 	/**
 	 * Request constructor.
@@ -56,38 +74,93 @@ class Request
 		$this->_url = $this->_clean_url($url);
 	}
 
+	/**
+	 * Clean the URL from any funny stuff.
+	 *
+	 * @param string $url
+	 */
 	private function _clean_url($url)
 	{
 		return $url;
 	}
 
+	/**
+	 * Access the request method.
+	 */
 	public function method()
 	{
 		return $this->_method;
 	}
 
+	/**
+	 * Access the request headers.
+	 *
+	 * @return \ArrayIterator over an array of arrays or strings
+	 */
 	public function headers()
 	{
 		return new \ArrayIterator($this->headers());
 	}
 
+	/**
+	 * Return the HTTP protocol version
+	 */
 	public function http_version()
 	{
 		return $this->_version;
 	}
 
+	/**
+	 * Retrieve the URL
+	 */
 	public function url()
 	{
 		return $this->_url;
 	}
 
+	/**
+	 * Retrieve base URL
+	 */
+	public function base_url()
+	{
+		return $this->_baseUrl;
+	}
+
+	/**
+	 * Return the action associated with the request
+	 */
+	public function action()
+	{
+		return $this->_action;
+	}
+
+	/**
+	 * Body of the request.
+	 */
 	public function body()
 	{
 		return $_body;
 	}
 
+	/**
+	 * Session associated with this request.
+	 * @return \clairity\Session
+	 */
 	public function session()
 	{
 		return $this->_session;
 	}
+
+	public function language()
+	{
+		return $this->_language;
+	}
+
+	/**
+	 * Whether this request is an XmlHttpRequest
+     */
+    public function is_xml_http_request()
+    {
+        return false;
+    }
 }
